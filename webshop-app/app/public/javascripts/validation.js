a1d9230e04715500551fc714a4c345d41c9441b2
Costin Lungu <costin.lungu@vodafone.com>
2021-12-13 15:34:57 +0200

    Intro form validation by CatalinT +refactoring css

--- webshop-app/app/public/javascripts/validation.js
+++ webshop-app/app/public/javascripts/validation.js
@@ -1,23 +1,55 @@
-// Example starter JavaScript for disabling form submissions if there are invalid fields
-(function () {
-  "use strict";
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll(".needs-validation");
+  const isValid = validateRegisterForm();
 
-  // Loop over them and prevent submission
-  Array.prototype.slice.call(forms).forEach(function (form) {
-    form.addEventListener(
-      "submit",
-      function (event) {
-        if (!form.checkValidity()) {
-          event.preventDefault();
-          event.stopPropagation();
-        }
+  const form = document.getElementById("form");
+  const errorElement = document.getElementById("error");
 
-        form.classList.add("was-validated");
-      },
-      false
+  if (isValid) {
+    // TODO: submit form
+  }
+});
+
+function validateRegisterForm() {
+  let isValid = true;
+
+  const username = document.getElementById("username");
+  removeAddInfiniteChildsOnSubmitBtnError(username.parentElement);
+  if (!username.value) {
+    isValid = false;
+    username.parentElement.insertAdjacentHTML(
+      "beforeend",
+      '<p class="error">Username is not valid</p>'
     );
-  });
-})();
+  } else if (username.value.length < 3 || username.value.length > 20) {
+    isValid = false;
+    username.parentElement.insertAdjacentElement(
+      "beforeend",
+      '<p class="error">Username is not valid</p>'
+    );
+  }
+
+  const email = document.getElementById("email");
+  const pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  const password = document.getElementById("password");
+  const confirmPassword = document.getElementById("confirmPassword");
+  if (password.value.length <= 6 || password.value.length >= 20) {
+    // TODO:
+  }
+  if (confirmPassword.value !== password.value) {
+    // TODO:
+  }
+  return isValid;
+}
+
+function removeAddInfiniteChildsOnSubmitBtnError(parent) {
+  const errors = parent.getElementsByClassName("error");
+
+  if (errors.length > 0) {
+    for (let errorChild of errors) {
+      parent.removeChild(errorChild);
+    }
+  }
+}
